503317bc1f6e6272a42d8fba5b6d2e905246cb2b
Luna Sarmiento <selene942008@hotmail.com>
2021-12-06 17:48:08 +0100

    about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,15 +1,28 @@
 const AboutMe = () => {
   return (
     <div className="about-me-container">
-      <div className="text-about-me-cont"></div>
-      <div className="video-about-me-cont">
-        <iframe
-          src="https://www.youtube.com/watch?v=oMltzYWxjS4&ab_channel=ShowItBetter"
-          width="560"
-          height="315"
-          frameborder="0"
-          allowfullscreen
-        ></iframe>
+      <h2 className="title-about-me">Conosciamoci meglio</h2>
+      <div className="about-me-inner-container">
+        <div className="text-about-me-cont">
+          <span>Ciao, sono Simone Risi...</span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className="video-about-me-cont">
+          <iframe
+          className="youtube-about-me"
+            src="https://www.youtube.com/embed/oMltzYWxjS4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,26 +14,26 @@ const TopCarousel = () => {
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="first-slide">
           <img
-            className="second-slide-img"
+            className="first-slide-img"
             src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/79dbd63251579.560c629f2eec8.jpg"
             alt="Second slide"
           />
 
-          <Carousel.Caption className="second-slide-caption">
+          <Carousel.Caption className="first-slide-caption">
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="first-slide">
           <img
-            className="third-slide-img"
+            className="first-slide-img"
             src="https://uploads-ssl.webflow.com/5894a32730554b620f7bf36d/5e848c30ae1ad81ed3ffa5df_5d14d33d86e4be76bae28c2e_What-Performance-Specs-Should-Your-PC-Have-to-Handle-3D-Rendering-EASY-RENDER.jpeg"
             alt="Third slide"
           />
 
-          <Carousel.Caption className="third-slide-caption">
+          <Carousel.Caption className="first-slide-caption">
             <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
